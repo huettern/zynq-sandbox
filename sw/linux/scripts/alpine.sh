b4e60c5c74a421954ab37a54b25f13b350598c88
Noah Huetter <noahhuetter@gmail.com>
2019-10-07 12:42:03 +0200

    Working and commented alpine script

--- sw/linux/scripts/alpine.sh
+++ sw/linux/scripts/alpine.sh
@@ -9,94 +9,166 @@ tools_url=$alpine_url/main/armv7/$tools_tar
 firmware_tar=linux-firmware-other-20181220-r0.apk
 firmware_url=$alpine_url/main/armv7/$firmware_tar
 
-linux_dir=tmp/linux-4.14
+linux_dir=../linux-4.14
 linux_ver=4.14.101-xilinx
+
+apline_root_src=../../alpine/root
+
+apline_uenv=../../alpine/uEnv.txt
 
 modules_dir=alpine-modloop/lib/modules/$linux_ver
 
+zip_dir=alpine-zip
+
 passwd=changeme
 
+echo "######################################################################"
+echo "# Download"
+# Alpine uboot
 test -f $uboot_tar || curl -L $uboot_url -o $uboot_tar
+# Alpine tools
 test -f $tools_tar || curl -L $tools_url -o $tools_tar
-
+# alpine linux firmware
 test -f $firmware_tar || curl -L $firmware_url -o $firmware_tar
 
+
+echo "######################################################################"
+echo "# Untar"
+# Additional drivers
 for tar in linux-firmware-ath9k_htc-20181220-r0.apk linux-firmware-brcm-20181220-r0.apk linux-firmware-rtlwifi-20181220-r0.apk
 do
   url=$alpine_url/main/armv7/$tar
   test -f $tar || curl -L $url -o $tar
 done
 
+# untar alpine uboot
 mkdir alpine-uboot
 tar -zxf $uboot_tar --directory=alpine-uboot
 
+# untar alpine tools
 mkdir alpine-apk
 tar -zxf $tools_tar --directory=alpine-apk --warning=no-unknown-keyword
+
+
+echo "######################################################################"
+echo "# Generate initramfs"
 
 mkdir alpine-initramfs
 cd alpine-initramfs
 
+# decompress tha vanilla alpine initramfs
 gzip -dc ../alpine-uboot/boot/initramfs-vanilla | cpio -id
+
+# Cleanup unwanted stuff
 rm -rf etc/modprobe.d
 rm -rf lib/firmware
 rm -rf lib/modules
 rm -rf var
+
+# repack
 find . | sort | cpio --quiet -o -H newc | gzip -9 > ../initrd.gz
 
 cd ..
 
+
+echo "######################################################################"
+echo "# Generate image for uboot"
 mkimage -A arm -T ramdisk -C gzip -d initrd.gz uInitrd
+
+
+echo "######################################################################"
+echo "# copy kernel"
 
 mkdir -p $modules_dir/kernel
 
+# Copy kernel modules from previously build linux kernel
 find $linux_dir -name \*.ko -printf '%P\0' | tar --directory=$linux_dir --owner=0 --group=0 --null --files-from=- -zcf - | tar -zxf - --directory=$modules_dir/kernel
-
 cp $linux_dir/modules.order $linux_dir/modules.builtin $modules_dir/
 
+# Generate dependencies
 depmod -a -b alpine-modloop $linux_ver
 
+
+
+echo "######################################################################"
+echo "# Additional kernel modules"
+
+# Copy seleced firmware binaries into modloop
 tar -zxf $firmware_tar --directory=alpine-modloop/lib/modules --warning=no-unknown-keyword --strip-components=1 --wildcards lib/firmware/ar* lib/firmware/rt*
 
+# Additional firmware
 for tar in linux-firmware-ath9k_htc-20181220-r0.apk linux-firmware-brcm-20181220-r0.apk linux-firmware-rtlwifi-20181220-r0.apk
 do
   tar -zxf $tar --directory=alpine-modloop/lib/modules --warning=no-unknown-keyword --strip-components=1
 done
 
+# Pack kernel modules and firmware
 mksquashfs alpine-modloop/lib modloop -b 1048576 -comp xz -Xdict-size 100%
 
+# cleanup
 rm -rf alpine-uboot alpine-initramfs initrd.gz alpine-modloop
+
+echo "######################################################################"
+echo "# Create root directory"
 
 root_dir=alpine-root
 
+# directory structure
 mkdir -p $root_dir/usr/bin
-cp /usr/bin/qemu-arm-static $root_dir/usr/bin/
-
 mkdir -p $root_dir/etc
-cp /etc/resolv.conf $root_dir/etc/
-
 mkdir -p $root_dir/etc/apk
+
+# apk cache
 mkdir -p $root_dir/media/mmcblk0p1/cache
 ln -s /media/mmcblk0p1/cache $root_dir/etc/apk/cache
 
-cp -r alpine/etc $root_dir/
-cp -r alpine/apps $root_dir/media/mmcblk0p1/
+# Copy contents from apline root dir into alpine-root.
+cp -r $apline_root_src/* $root_dir/
 
-for project in led_blinker sdr_receiver_hpsdr sdr_transceiver sdr_transceiver_emb sdr_transceiver_ft8 sdr_transceiver_hpsdr sdr_transceiver_wide sdr_transceiver_wspr mcpha vna
-do
-  mkdir -p $root_dir/media/mmcblk0p1/apps/$project
-  cp -r projects/$project/server/* $root_dir/media/mmcblk0p1/apps/$project/
-  cp -r projects/$project/app/* $root_dir/media/mmcblk0p1/apps/$project/
-  cp tmp/$project.bit $root_dir/media/mmcblk0p1/apps/$project/
-done
-
+# Copy alpine binary and qemu arm CPU emulator to install alpine. 
+# Further, for the chroot environment to find the alpine servers, our hosts resolv config is copied.
 cp -r alpine-apk/sbin $root_dir/
+cp /usr/bin/qemu-arm-static $root_dir/usr/bin/
+cp /etc/resolv.conf $root_dir/etc/
 
-chroot $root_dir /sbin/apk.static --repository $alpine_url/main --update-cache --allow-untrusted --initdb add alpine-base
 
+
+# DELETED
+# cp -r alpine/etc $root_dir/
+# cp -r alpine/apps $root_dir/media/mmcblk0p1/
+# cp -r alpine-apk/sbin $root_dir/
+# rc-update add avahi-daemon default
+# rc-update add chronyd default
+# rc-update add dhcpcd default
+# mkdir -p etc/runlevels/wifi
+# rc-update -s add default wifi
+
+# rc-update add iptables wifi
+# rc-update add dnsmasq wifi
+# rc-update add hostapd wifi
+
+# sed -i 's/^SAVE_ON_STOP=.*/SAVE_ON_STOP="no"/;s/^IPFORWARD=.*/IPFORWARD="yes"/' etc/conf.d/iptables
+
+# DELETED
+
+echo "######################################################################"
+echo "# Install alpine base"
+
+# Chroot into alpine and install alpine-base
+sudo chroot $root_dir /sbin/apk.static \
+  --repository $alpine_url/main \
+  --update-cache --allow-untrusted --initdb \
+  add alpine-base
+
+# Create a repositories file for upstream repository path.
 echo $alpine_url/main > $root_dir/etc/apk/repositories
 echo $alpine_url/community >> $root_dir/etc/apk/repositories
 
-chroot $root_dir /bin/sh <<- EOF_CHROOT
+echo "######################################################################"
+echo "# chroot for more settings"
+
+# Now we chroot into the alpine-base installation and complete further installations.
+sudo chroot $root_dir /bin/sh <<- EOF_CHROOT
 
 apk update
 apk add haveged openssh ucspi-tcp6 iw wpa_supplicant dhcpcd dnsmasq hostapd iptables avahi dbus dcron chrony gpsd libgfortran musl-dev fftw-dev libconfig-dev alsa-lib-dev alsa-utils curl wget less nano bc dos2unix
@@ -118,22 +190,10 @@ ln -s /etc/init.d/devfs etc/runlevels/sysinit/devfs
 ln -s /etc/init.d/dmesg etc/runlevels/sysinit/dmesg
 ln -s /etc/init.d/mdev etc/runlevels/sysinit/mdev
 
-rc-update add avahi-daemon default
-rc-update add chronyd default
-rc-update add dhcpcd default
 rc-update add local default
 rc-update add dcron default
 rc-update add haveged default
 rc-update add sshd default
-
-mkdir -p etc/runlevels/wifi
-rc-update -s add default wifi
-
-rc-update add iptables wifi
-rc-update add dnsmasq wifi
-rc-update add hostapd wifi
-
-sed -i 's/^SAVE_ON_STOP=.*/SAVE_ON_STOP="no"/;s/^IPFORWARD=.*/IPFORWARD="yes"/' etc/conf.d/iptables
 
 sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' etc/ssh/sshd_config
 
@@ -142,48 +202,44 @@ echo root:$passwd | chpasswd
 setup-hostname red-pitaya
 hostname red-pitaya
 
-sed -i 's/^# LBU_MEDIA=.*/LBU_MEDIA=mmcblk0p1/' etc/lbu/lbu.conf
-
 cat <<- EOF_CAT > root/.profile
 alias rw='mount -o rw,remount /media/mmcblk0p1'
 alias ro='mount -o ro,remount /media/mmcblk0p1'
 EOF_CAT
 
-ln -s /media/mmcblk0p1/apps root/apps
-ln -s /media/mmcblk0p1/wifi root/wifi
+sed -i 's/^# LBU_MEDIA=.*/LBU_MEDIA=mmcblk0p1/' etc/lbu/lbu.conf
 
 lbu add root
 lbu delete etc/resolv.conf
-lbu delete etc/periodic/ft8
-lbu delete etc/periodic/wspr
 lbu delete root/.ash_history
-
 lbu commit -d
-
-apk add subversion make gcc gfortran
-
-for project in server sdr_receiver_hpsdr sdr_transceiver sdr_transceiver_emb sdr_transceiver_ft8 sdr_transceiver_hpsdr sdr_transceiver_wide mcpha vna
-do
-  make -C /media/mmcblk0p1/apps/\$project clean
-  make -C /media/mmcblk0p1/apps/\$project
-done
-
-svn co svn://svn.code.sf.net/p/wsjt/wsjt/wsjtx/trunk/lib/wsprd /media/mmcblk0p1/apps/sdr_transceiver_wspr/wsprd
-make -C /media/mmcblk0p1/apps/sdr_transceiver_wspr/wsprd CFLAGS='-O3 -march=armv7-a -mtune=cortex-a9 -mfpu=neon -mfloat-abi=hard -ffast-math -fsingle-precision-constant -mvectorize-with-neon-quad' wsprd
-make -C /media/mmcblk0p1/apps/sdr_transceiver_wspr
 
 EOF_CHROOT
 
-cp -r $root_dir/media/mmcblk0p1/apps .
-cp -r $root_dir/media/mmcblk0p1/cache .
-cp $root_dir/media/mmcblk0p1/red-pitaya.apkovl.tar.gz .
+echo "######################################################################"
+echo "# restore hostname"
 
-cp -r alpine/wifi .
+sudo hostname -F /etc/hostname
 
-hostname -F /etc/hostname
 
-rm -rf $root_dir alpine-apk 
+echo "######################################################################"
+echo "# zip stuff"
 
-zip -r red-pitaya-alpine-3.9-armv7-`date +%Y%m%d`.zip apps boot.bin cache devicetree.dtb modloop red-pitaya.apkovl.tar.gz uEnv.txt uImage uInitrd wifi
+mkdir -p $zip_dir
 
-rm -rf apps cache modloop red-pitaya.apkovl.tar.gz uInitrd wifi
+cp ../boot.bin $zip_dir/
+cp ../uImage $zip_dir/
+cp ../devicetree.dtb $zip_dir/
+cp ../uEnv.txt $zip_dir/
+
+cp -r $root_dir/media/mmcblk0p1/cache $zip_dir/
+cp $root_dir/media/mmcblk0p1/red-pitaya.apkovl.tar.gz $zip_dir/
+cp modloop $zip_dir/
+cp uInitrd $zip_dir/
+
+cp $apline_uenv $zip_dir/
+
+zip -r red-pitaya-alpine-3.9-armv7-`date +%Y%m%d`.zip $zip_dir/
+
+echo "######################################################################"
+echo "# DONE!"
